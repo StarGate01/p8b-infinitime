d58258d9aed180042d628cfff2eec905d53b7a35
Christoph Honal <christoph.honal@web.de>
2022-03-18 02:19:42 +0100

    Improve scripts and vscode integration

--- scripts/load_infinitime.sh
+++ scripts/load_infinitime.sh
@@ -3,4 +3,4 @@
 cd "${0%/*}"
 
 cd ../InfiniTime
-bootloader/ota-dfu-python/dfu.py -z "build/src/pinetime-mcuboot-app-dfu-1.3.0.zip" -a D5:DF:61:E5:8F:CE --legacy                           
+bootloader/ota-dfu-python/dfu.py -z "build/src/pinetime-mcuboot-app-dfu-1.3.0.zip" -a D5:DF:61:E5:8F:CE --legacy
