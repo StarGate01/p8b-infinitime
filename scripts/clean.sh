6c8cb5c50bd69d7a2e07f3fe624ebced4147eec8
Christoph Honal <christoph.honal@web.de>
2022-05-10 16:06:49 +0200

    Fix clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,6 +5,6 @@ cd "${0%/*}"
 rm -rf ../pinetime-mcuboot-bootloader/bin
 rm -rf ../pinetime-mcuboot-bootloader/reloader/build-*
 rm -rf ../InfiniTime/build
-rm -rf ../wasp-os/build-p8
+rm -rf ../wasp-os/build-*
 rm -rf ../wasp-os/bootloader/_build*
 rm -rf ../wasp-os/reloader/build-*
