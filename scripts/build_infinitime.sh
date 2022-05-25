fccfc449d087e4206384ff4f450507b724c5f83b
Christoph Honal <christoph.honal@web.de>
2022-05-25 16:12:37 +0200

    Update submodule

--- scripts/build_infinitime.sh
+++ scripts/build_infinitime.sh
@@ -10,7 +10,7 @@ cd build
 
 COMMON_OPTIONS="-DCMAKE_BUILD_TYPE=Release -DARM_NONE_EABI_TOOLCHAIN_PATH=/usr -DNRF5_SDK_PATH=/opt/nrf5-sdk -DUSE_JLINK=1 -DNRFJPROG=/usr/bin/nrfjprog -DBUILD_DFU=1" 
 if [ "$TARGET" = "pinetime" ]; then
-    cmake -DDRIVER_TOUCH=DYNAMIC -DTARGET_DEVICE=PINETIME -DLF_CLK=XTAL -DDRIVER_ACC=BMA421 $COMMON_OPTIONS ..
+    cmake -DTARGET_DEVICE=PINETIME $COMMON_OPTIONS ..
 elif [ "$TARGET" = "p8" ] || [ "$TARGET" = "p8b" ]; then
     cmake -DTARGET_DEVICE=P8B $COMMON_OPTIONS ..
 elif [ "$TARGET" = "p8a" ]; then
