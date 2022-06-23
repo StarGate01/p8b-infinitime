df2a2c36cf9f53199ee08cef73f7669be132a2a5
Christoph Honal <christoph.honal@web.de>
2022-06-23 02:48:14 +0200

    Update flakes

--- flake.nix
+++ flake.nix
@@ -2,14 +2,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     nixpkgs-fontconv.url = "github:jvanbruegge/nixpkgs/lv_font_conv";
-    nixpkgs-pyanrfutil.url = "github:StarGate01/nixpkgs/pyanrfutil";
+    nixpkgs-pyanrfutil.url = "github:StarGate01/nixpkgs/pyanrfutil-fix";
+    nixpkgs-nrf.url = "github:StarGate01/nixpkgs/nrf-command-line-tools";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-fontconv, nixpkgs-pyanrfutil }:
+  outputs = { self, nixpkgs, nixpkgs-fontconv, nixpkgs-pyanrfutil, nixpkgs-nrf }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       pkgs-fontconv = nixpkgs-fontconv.legacyPackages.x86_64-linux;
       pkgs-pyanrfutil = nixpkgs-pyanrfutil.legacyPackages.x86_64-linux;
+      pkgs-nrf = import nixpkgs-nrf {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        config.segger-jlink.acceptLicense = true;
+      };
     in
     {
       devShell.x86_64-linux =
@@ -24,6 +30,8 @@
             pkgs-fontconv.nodePackages.lv_font_conv
             mynewt-newt
             cmake
+            openocd
+            pkgs-nrf.nrf-command-line-tools
             (python3.withPackages (ps: with ps; [
               cbor
               click
