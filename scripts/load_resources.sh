2a5875a57d83186209bfb51959a3a6781882eb46
Christoph Honal <christoph.honal@web.de>
2023-01-29 17:03:56 +0100

    Use ITD to load resources

--- scripts/load_resources.sh
+++ scripts/load_resources.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ADDR=${1:-"D5:DF:61:E5:8F:CE"}
-echo "Using address $ADDR"
 cd "${0%/*}"
 
 source ./infinitime_version.sh
 
 cd ../InfiniTime
+
+itctl res load "build/src/resources/infinitime-resources-$INFINITIME_VERSION.zip"
